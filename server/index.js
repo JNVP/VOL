f46138ba4038030eac014f774c40574eaeea95ca
Vinit Patel <vinit@Vinits-MacBook-Pro.local>
2021-06-08 08:39:16 -0400

    path module included

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 // server/index.js
-
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
