3eb547a595dbc81144207e77062e07b6c5f2bdb0
Vinit Patel <vinit@Vinits-MacBook-Pro.local>
2021-06-08 08:34:20 -0400

    path fix

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
